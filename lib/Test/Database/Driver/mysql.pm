f07ff18669c307ae4baec00f12db438eeba6fa9e
Philippe Bruhat (BooK) <book@cpan.org>
2009-03-29 18:23:34 +0200

    Test::Database::Driver::mysql: complete rewrite

--- lib/Test/Database/Driver/mysql.pm
+++ lib/Test/Database/Driver/mysql.pm
@@ -2,111 +2,55 @@ package Test::Database::Driver::mysql;
 use strict;
 use warnings;
 
+use DBI;
+
 use Test::Database::Driver;
 our @ISA = qw( Test::Database::Driver );
 
-use File::Spec;
-use DBI;
-
-sub setup_engine {
-    my ($class) = @_;
-    my $dir = $class->base_dir();
-    my $config;
-
-    # is there a my.cnf file already?
-    my $cnf = File::Spec->catfile( $dir, 'my.cnf' );
-    if ( !-e $cnf ) {
-        open my $fh, '>', $cnf or die "Unable to open $cnf for writing: $!";
-        $config = {
-            datadir    => $dir,
-            socket     => File::Spec->catfile( $dir, 'mysqld.sock' ),
-            'pid-file' => File::Spec->catfile( $dir, 'mysqld.pid' ),
-            port       => $class->available_port(),
-            cnf        => $cnf,
-        };
-        print {$fh} << "CNF";
-[mysqld]
-datadir   = $config->{datadir}
-socket    = $config->{socket}
-pid-file  = $config->{'pid-file'}
-port      = $config->{port}
-log-error = mysqld.err
-[client]
-socket    = $config->{socket}
-port      = $config->{port}
-CNF
-        close $fh;
-    }
-    else {
-
-        # read the file we just wrote
-        # FIXME - should I depend on Config::Tiny for this?
-        open my $fh, $cnf or die "Unable to open $cnf for reading: $!";
-
-        $config = { cnf => $cnf };
-        my $section;
-        while (<$fh>) {
-            chomp;
-            if (/\[(\w+)\]/) { $section = $1 }
-            elsif ( $section eq 'mysqld' && /([-\w]+)\s*=\s*(.*)/ ) {
-                $config->{$1} = $2;
-            }
-        }
-        close $fh;
-    }
-
-    # assume system tables are here if 'mysql' directory exists
-    if ( !-e File::Spec->catdir( $class->base_dir(), 'mysql' ) ) {
-        $class->run_cmd( 'mysql_install_db', "--defaults-file=$cnf" );
-    }
-
-    # return the configuration information
-    return $config;
+sub _version {
+    return DBI->connect( $_[0]->connection_info() )
+        ->selectcol_arrayref('SELECT VERSION()')->[0];
 }
 
-sub start_engine {
-    my ( $class, $config ) = @_;
-
-    # is the server already started?
-    if ( !-e $config->{'pid-file'} || !-e $config->{socket} ) {
-
-        # spawn a new mysqld process (must fork)
-        $class->spawn_cmd( 'mysqld_safe', "--defaults-file=$config->{cnf}" );
-
-        # wait until the server has started
-        my $i = 0;
-        sleep 1 while !-e $config->{socket} && $i++ < 10;
-    }
-
-    return $config;
+sub _dsn {
+    return 'dbi:mysql:' . join ';',
+        map {"$_=$_[0]->{$_}"} grep { exists $_[0]->{$_} } qw( host port );
 }
 
-sub stop_engine {
-    my ( $class, $config ) = @_;
-
-    $class->run_cmd(
-        'mysqladmin',  "--defaults-file=$config->{cnf}",
-        '--user=root', 'shutdown'
-    );
-}
+sub essentials { qw< host port username password > }
 
 sub create_database {
-    my ( $class, $config, $dbname ) = @_;
+    my ( $self, $dbname ) = @_;
+    $dbname ||= $self->available_dbname();
 
-    my $dsn = join ';', 'dbi:mysql:host=localhost', "port=$config->{port}",
-        "mysql_read_default_file=$config->{cnf}";
-
-    # check if the requested database exists
-    if ( !-e File::Spec->catdir( $class->base_dir(), $dbname ) ) {
-        my $dbh = DBI->connect( $dsn, 'root', '' );
-        $dbh->do("CREATE DATABASE $dbname");
-    }
+    # create the database if it doesn't exist
+    $self->drh()
+        ->func( 'createdb', $dbname, $self->connection_info(), 'admin' )
+        if !grep { $_ eq $dbname } $self->databases();
 
     # return the handle
     return Test::Database::Handle->new(
-        dsn      => "$dsn;database=$dbname",
-        username => $class->username(),
+        dsn      => $self->dsn() . ";database=$dbname",
+        name     => $dbname,
+        username => $self->username(),
+        password => $self->password(),
+        driver   => $self,
     );
+}
+
+sub drop_database {
+    my ( $self, $dbname ) = @_;
+
+    # drop the database if it exists
+    $self->drh()->func( 'dropdb', $dbname, $self->connection_info(), 'admin' )
+        if grep { $_ eq $dbname } $self->databases();
+}
+
+sub databases {
+    my ($self) = @_;
+    my $databases = DBI->connect( $self->connection_info() )
+        ->selectall_arrayref('SHOW DATABASES');
+    return grep { $_ ne 'information_schema' } map {@$_} @$databases;
 }
 
 'mysql';
